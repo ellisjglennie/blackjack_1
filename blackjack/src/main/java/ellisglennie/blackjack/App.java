1a0ca204bf555840bcb8dcd35003640639905aeb
A <ellisjglennie>
2018-09-18 15:04:12 -0400

    init: created maven project
    -added Maven dependency

---
+++ blackjack/src/main/java/ellisglennie/blackjack/App.java
@@ -0,0 +1,13 @@
+package ellisglennie.blackjack;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ blackjack/src/test/java/ellisglennie/blackjack/AppTest.java
@@ -0,0 +1,20 @@
+package ellisglennie.blackjack;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
